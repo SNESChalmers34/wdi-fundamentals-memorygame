2c289369304fdec3db01d315f845d8b463e28bd1
Mark Sweet <marks828@gmail.com>
2016-09-04 16:53:55 -0400

    updated index and main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,12 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = 'king';
+var cardTwo = 'queen';
+var cardThree = 'queen';
+var cardFour = 'king';
+
+if (cardOne === cardFour){
+	alert('You found a Match!')
+} else{
+	alert ('Sorry, try again.')
+}
